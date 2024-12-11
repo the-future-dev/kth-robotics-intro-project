c450f01fb5aa6c0e93b4d61024b474369b62d217
the-future-dev <andre.ritossa@gmail.com>
2024-12-11 22:17:16 +0100

    try

--- robotics_project/scripts/behaviour_trees/pick_and_place_behaviours.py
+++ robotics_project/scripts/behaviour_trees/pick_and_place_behaviours.py
@@ -2,18 +2,19 @@
 
 import py_trees as pt, py_trees_ros as ptr, rospy
 from geometry_msgs.msg import Twist, PoseStamped
-#from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
-from tiago_pick_demo.msg import PickUpPoseAction, PickUpPoseGoal, PickUpPoseResult, PickUpPoseFeedback
+from tiago_pick_demo.msg import PickUpPoseAction, PickUpPoseGoal
 from robotics_project.srv import MoveHead
 from actionlib import SimpleActionClient
 from std_srvs.srv import Empty
+import tf2_ros
+from tf2_geometry_msgs import do_transform_pose
 
 class DetectCube(pt.behaviour.Behaviour):
-    """Detects the cube using ArUco marker detection"""
-    
     def __init__(self):
         super(DetectCube, self).__init__("Detect Cube")
         self.detected_pose = None
+        self.tfBuffer = tf2_ros.Buffer()
+        self.tf_l = tf2_ros.TransformListener(self.tfBuffer)
         
     def setup(self, timeout):
         self.aruco_subscriber = rospy.Subscriber(
@@ -24,16 +25,44 @@ class DetectCube(pt.behaviour.Behaviour):
         return True
         
     def aruco_callback(self, msg):
-        self.detected_pose = msg
-        
+        try:
+            # Transform pose to base_footprint frame
+            transform = self.tfBuffer.lookup_transform(
+                "base_footprint",
+                msg.header.frame_id,
+                rospy.Time(0)
+            )
+            self.detected_pose = do_transform_pose(msg, transform)
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, 
+                tf2_ros.ExtrapolationException) as e:
+            rospy.logwarn(f"TF Error: {e}")
+            
     def update(self):
         if self.detected_pose is None:
+            rospy.loginfo("Waiting for cube detection...")
             return pt.common.Status.RUNNING
+        rospy.loginfo("Cube detected!")
+        return pt.common.Status.SUCCESS
+
+class NavigateToPosition(pt.behaviour.Behaviour):
+    def __init__(self, position_name, x, y, theta):
+        super(NavigateToPosition, self).__init__(f"Navigate to {position_name}")
+        self.position_name = position_name
+        self.target_x = x
+        self.target_y = y
+        self.target_theta = theta
+        self.cmd_vel_pub = rospy.Publisher('/key_vel', Twist, queue_size=1)
+        
+    def update(self):
+        # Simple navigation using position-based control
+        # In real implementation, should use move_base or similar
+        msg = Twist()
+        msg.linear.x = self.target_x
+        msg.angular.z = self.target_theta
+        self.cmd_vel_pub.publish(msg)
         return pt.common.Status.SUCCESS
 
 class PickCube(pt.behaviour.Behaviour):
-    """Executes the pick action"""
-    
     def __init__(self):
         super(PickCube, self).__init__("Pick Cube")
         self.pick_client = SimpleActionClient('/pickup_pose', PickUpPoseAction)
@@ -42,23 +71,25 @@ class PickCube(pt.behaviour.Behaviour):
         return self.pick_client.wait_for_server(timeout=rospy.Duration(timeout))
         
     def update(self):
-        if not self.detected_pose:
+        if not hasattr(self.parent, 'detected_pose'):
+            rospy.logerr("No cube pose available!")
             return pt.common.Status.FAILURE
             
         goal = PickUpPoseGoal()
-        goal.object_pose = self.detected_pose
+        goal.object_pose = self.parent.detected_pose
         
+        rospy.loginfo("Executing pick action...")
         self.pick_client.send_goal(goal)
         self.pick_client.wait_for_result()
         
         result = self.pick_client.get_result()
-        if result.error_code == 1:  # SUCCESS
+        if result.error_code == 1:
+            rospy.loginfo("Pick successful!")
             return pt.common.Status.SUCCESS
+        rospy.logerr("Pick failed!")
         return pt.common.Status.FAILURE
 
 class PlaceCube(pt.behaviour.Behaviour):
-    """Executes the place action"""
-    
     def __init__(self):
         super(PlaceCube, self).__init__("Place Cube")
         self.place_client = SimpleActionClient('/place_pose', PickUpPoseAction)
@@ -68,14 +99,30 @@ class PlaceCube(pt.behaviour.Behaviour):
         
     def update(self):
         goal = PickUpPoseGoal()
-        # Set place pose slightly above current position
-        goal.object_pose = self.detected_pose
-        goal.object_pose.pose.position.z += 0.05
+        # Get place pose from parameters
+        place_pose = rospy.get_param("~place_pose")
+        goal.object_pose = self._create_pose_stamped(place_pose)
         
+        rospy.loginfo("Executing place action...")
         self.place_client.send_goal(goal)
         self.place_client.wait_for_result()
         
         result = self.place_client.get_result()
-        if result.error_code == 1:  # SUCCESS
+        if result.error_code == 1:
+            rospy.loginfo("Place successful!")
             return pt.common.Status.SUCCESS
+        rospy.logerr("Place failed!")
         return pt.common.Status.FAILURE
+        
+    def _create_pose_stamped(self, pose_array):
+        pose = PoseStamped()
+        pose.header.frame_id = "base_footprint"
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position.x = pose_array[0]
+        pose.pose.position.y = pose_array[1]
+        pose.pose.position.z = pose_array[2]
+        pose.pose.orientation.x = pose_array[3]
+        pose.pose.orientation.y = pose_array[4]
+        pose.pose.orientation.z = pose_array[5]
+        pose.pose.orientation.w = pose_array[6]
+        return pose
--- robotics_project/scripts/behaviour_trees/pick_and_place_tree.py
+++ robotics_project/scripts/behaviour_trees/pick_and_place_tree.py
@@ -2,48 +2,60 @@
 
 import py_trees as pt, py_trees_ros as ptr, rospy
 from behaviours_student import *
-from pick_and_place_behaviours import DetectCube, PickCube, PlaceCube
+from pick_and_place_behaviours import *
 from reactive_sequence import RSequence
 
 class PickAndPlaceBehaviourTree(ptr.trees.BehaviourTree):
-
     def __init__(self):
         rospy.loginfo("Initialising pick and place behaviour tree")
 
         # Create behaviors
         detect_cube = DetectCube()
-        pick_cube = PickCube()
-        move_to_place = pt.composites.Selector(
-            name="Move to place position",
-            children=[
-                counter(10, "At place position?"),
-                go("Go to place position!", 1, 0)
-            ]
-        )
-        place_cube = PlaceCube()
         
-        # Return to start position if place fails
-        return_to_start = pt.composites.Selector(
-            name="Return to start",
-            children=[
-                counter(10, "At start?"),
-                go("Return to start!", -1, 0)
-            ]
-        )
+        # Navigation behaviors
+        nav_to_pick = NavigateToPosition("pick position", -1.1480, -6.1, -0.709)
+        nav_to_place = NavigateToPosition("place position", 2.6009, -1.7615, 0.0)
+        nav_to_start = NavigateToPosition("start position", -1.1480, -6.1, -0.709)
+        
+        # Pick and place behaviors
+        pick_cube = PickCube()
+        place_cube = PlaceCube()
 
-        # Create main sequence
+        # Create the main task sequence
         main_sequence = RSequence(
-            name="Pick and Place Sequence",
+            name="Pick and Place Task",
             children=[
+                # 1. Detect cube
                 detect_cube,
+                
+                # 2. Navigate to pick position and pick
+                nav_to_pick,
                 pick_cube,
-                move_to_place,
+                
+                # 3. Carry cube to second table
+                nav_to_place,
+                
+                # 4. Place cube
                 place_cube,
-                pt.decorators.FailureIsSuccess(
-                    name="Place Success Check",
+                
+                # 5. Check result and retry if needed
+                pt.decorators.FailureIsRunning(
                     child=pt.composites.Selector(
-                        name="Place Result",
-                        children=[place_cube, return_to_start]
+                        name="Task Completion Check",
+                        children=[
+                            place_cube,  # Try placing again
+                            RSequence(   # If placing fails, return to start and retry
+                                name="Return and Retry",
+                                children=[
+                                    nav_to_start,
+                                    detect_cube,
+                                    nav_to_pick,
+                                    pick_cube,
+                                    nav_to_place,
+                                    place_cube
+                                ]
+                            )
+                        ]
                     )
                 )
             ]
